6f07bd92f784b2bae43c2654894e48ea40351b8c
Anton <antonhellbe2@gmail.com>
2018-01-29 18:06:10 +0100

    Changes to playlistform, added validation

--- src/components/modal/PlaylistForm.js
+++ src/components/modal/PlaylistForm.js
@@ -4,36 +4,69 @@ import { Field, reduxForm } from 'redux-form';
 
 class PlaylistForm extends Component {
 
+    renderField = ({ input, label, type, labelFor, meta: { touched, error } }) => (
+        <div className="input-field">
+            <label htmlFor={ labelFor }>{label}</label>
+            <div className="input-field-wrapper">
+                <input { ...input } placeholder={ label } type={ type } />
+                {touched && <p>{error} </p>}
+            </div>
+        </div>
+    )
+
+    renderSelect = ({ input, label, type, labelFor, meta: { touched, error }, children }) => (
+        <div className="input-field">
+            <label htmlFor={ labelFor }>{label}</label>
+            <div className="input-field-wrapper">
+                <select { ...input } type={ type }>
+                    {children}
+                </select>
+                {touched && (error && <p> {error} </p>)}
+            </div>
+        </div>
+    )
+
     render() {
         const {
             initialValues,
-            create
+            create,
         } = this.props;
+        console.log(this.props);
         return (
             <form
                 className="edit-playlist-form"
                 initialvalues={ initialValues }
                 onSubmit={ this.props.handleSubmit }
             >
-                <div className="input-field">
-                    <label htmlFor="name">Name</label>
-                    <Field name="name" component="input" type="text" />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="publicPlaylist">Public</label>
-                    <Field name="publicPlaylist" component="select">
-                        <option value="true">True</option>
-                        <option value="false">False</option>
-                    </Field>
-                </div>
+                <Field
+                    name="name"
+                    type="text"
+                    component={ this.renderField }
+                    label="Name"
+                    labelFor="name"
+                />
 
-                <div className="input-field">
-                    <label htmlFor="collaborative">Collaborative</label>
-                    <Field name="collaborative" component="select">
-                        <option value="true">True</option>
-                        <option value="false">False</option>
-                    </Field>
-                </div>
+                <Field
+                    name="publicPlaylist"
+                    type="select"
+                    component={ this.renderSelect }
+                    label="public"
+                    labelFor="publicPlaylist"
+                >
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </Field>
+
+                <Field
+                    name="collaborative"
+                    type="select"
+                    component={ this.renderSelect }
+                    label="collaborative"
+                    labelFor="collaborative"
+                >
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </Field>
 
                 <div className="input-field">
                     <label htmlFor="description">Description</label>
@@ -43,25 +76,39 @@ class PlaylistForm extends Component {
                         type="text"
                     />
                 </div>
-                { !create ?
-                (
-                <button type="submit">
-                    Update
+                {!create ?
+                    (
+                        <button type="submit">
+                            Update
                 </button>
-                ) : 
-                (
-                    <button type="submit">
-                        Create
+                    ) :
+                    (
+                        <button type="submit">
+                            Create
                     </button>
-                )
+                    )
                 }
             </form>
         );
     }
 }
 
+const validateInput = (formProps) => {
+    const errors = {};
+    if (formProps.name.length < 3) {
+        errors.name = 'Needs to be 3 characters or more';
+    }
+    if (formProps.publicPlaylist === 'true' && formProps.collaborative === 'true') {
+        console.log('Both public and collaborative true');
+        errors.publicPlaylist = 'Playlist needs to be private if it\'s collaborative';
+    }
+
+    return errors;
+};
+
 const Playlist = reduxForm({
-    enableReinitialize: true
+    enableReinitialize: true,
+    validate: validateInput
 })(PlaylistForm);
 
 export default Playlist;
